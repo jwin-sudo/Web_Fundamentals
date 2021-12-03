bdfc144d84f1617e3067a1add615596bc9d55e02
jwin-sudo <77118008+jwin-sudo@users.noreply.github.com>
2021-12-03 15:54:24 -0600

    Add files via upload

---
+++ 1-HTML-CSS-JavaScript/js/dom-patterns.js
@@ -0,0 +1,25 @@
+function showPattern(){
+    const colorArr = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+    let topPosition = 25;
+    let leftPosition = 25;
+    let width = 500;
+    let height = 500;
+
+    while (width > 50){
+        const randomColorIdx = Math.floor(Math.random()* colorArr.length);
+        const newDiv = document.createElement('div');
+
+        newDiv.style.top = topPosition + 'px';
+        newDiv.style.left = leftPosition + 'px';
+        newDiv.style.width = width + 'px';
+        newDiv.style.height = height + 'px';
+        newDiv.style.background = colorArr[randomColorIdx];
+        
+        document.body.appendChild(newDiv);
+        topPosition +=10;
+        leftPosition +=10;
+        width -=20;
+        height -=20;
+    }
+}
